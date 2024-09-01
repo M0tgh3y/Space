03c997b17235b3383b477d28adc65a62d930c4bb
Motahare <149566082+M0tgh3y@users.noreply.github.com>
2024-09-01 13:00:30 +0330

    add diffrent file for mobile view

--- space.js
+++ space.js
@@ -1,8 +1,3 @@
-// Get the hamburger menu icon and navigation menu container
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const navMenu = document.querySelector('.nav-menu');
-
-// Add event listener to toggle the menu
-hamburgerMenu.addEventListener('click', () => {
-    navMenu.classList.toggle('show');
-});
+if (window.innerWidth < 768) {
+    window.location.href = 'mobile.html';
+}
